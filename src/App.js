b6c6bc8cf0e0913fe4dd53181d7711a41a5debb1
jcunanan05 <j_cunanan05@yahoo.com>
2019-09-07 16:38:54 -0400

    Add hello world code
    testing out if initial code is fetching firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import firebase from "firebase";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    selectedNoteIndex: null,
+    selectedNote: null,
+    notes: null
+  };
+
+  render() {
+    return <div>Hello World</div>;
+  }
+
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection("notes")
+      .onSnapshot(serverUpdate => {
+        const notes = serverUpdate.docs.map(_doc => {
+          const data = _doc.data();
+          data["id"] = _doc.id;
+
+          return data;
+        });
+
+        console.log(notes);
+        this.setState({ notes });
+      });
+  };
 }
 
 export default App;
